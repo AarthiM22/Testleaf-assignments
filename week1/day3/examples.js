46a162503454e09e2f0eb5088bd246c85ec72a49
AarthiM22 <aarthi.pvg@gmail.com>
2026-08-06 19:37:24 +0530

    Add files via upload

---
+++ week1/day3/examples.js
@@ -0,0 +1,12 @@
+function number1(){
+    for (var a = 0; a <= 25; a++)
+         {
+        for (let b = 0; b <= 25; b++) 
+            {
+            if(a+b == 25){
+            console.log(a+" + "+b +" is ",25);
+            }
+        }
+        
+    }
+}number1();
---
+++ week1/day3/oddnumber.js
@@ -0,0 +1,14 @@
+
+function printOddnumbers(){
+    
+
+for (let number = 1; number <= 25; number++) {
+     if (number%2 != 0){
+        console.log('OddNumber is ' ,number);
+    
+
+     }
+
+    
+}
+} printOddnumbers();
